508c61e94b5eedb99994b5760f18354cfd5c174b
Bhekani Khumalo <bhekanik@gmail.com>
2021-10-22 12:48:14 +0100

    Change the way recent queries are saved
    Recent queries currently save the final string that is sent to the
    search engine. This limits what we can do with them later. This changes
    this so that we now save the whole filters object together with the
    search term so that we can essentially bring the query back to life if
    we want to and allow the user to edit it before sending it again.

--- src/lib/PastFilters.svelte
+++ src/lib/PastFilters.svelte
@@ -1,9 +1,52 @@
 <script lang="ts">
 	import { recentQueries } from '../stores/recentQueries';
+
+	export const generateQuery = (filters: Record<string, string>): string =>
+		Object.values(filters).reduce((prev, curr) => `${prev}${curr}`);
 </script>
 
-<ul>
+<h3 class="heading">Recent Queries</h3>
+<div class="query-list">
 	{#each $recentQueries as query}
-		<li>{query}</li>
+		<div class="query">
+			<h3 class="query_search-term">{query.searchTerm}</h3>
+			<span class="label">Filters: </span>
+			<span class="filters">
+				{generateQuery(query.filters)}
+			</span>
+		</div>
 	{/each}
-</ul>
+</div>
+
+<style lang="scss">
+	.heading {
+		font-size: 1.5rem;
+		padding: 1rem 0 0.5rem;
+		border-top: 1px solid gray;
+	}
+
+	.query-list {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+
+		.label {
+			color: gray;
+		}
+	}
+
+	.query {
+		border: 1px solid gray;
+		border-radius: 5px;
+		padding: 1rem 2rem;
+	}
+
+	.query_search-term {
+		font-size: 1.5rem;
+		margin-bottom: 0.5rem;
+	}
+
+	.filters {
+		font-weight: bold;
+	}
+</style>
--- src/stores/recentQueries.ts
+++ src/stores/recentQueries.ts
@@ -1,6 +1,11 @@
 import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
-export const recentQueries: Writable<string[]> = writable(
+export interface Query {
+	searchTerm: string;
+	filters: Record<string, string>;
+}
+
+export const recentQueries: Writable<Query[]> = writable(
 	JSON.parse(globalThis.localStorage?.getItem('recentQueries') || '[]')
 );
