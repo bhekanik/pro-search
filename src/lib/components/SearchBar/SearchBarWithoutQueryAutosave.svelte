867cfbcb104528dcb61d8fcd95aa82a3be50f4c2
Guide Fari <guideg6@gmail.com>
2021-12-29 21:11:53 +0200

    feat: clear search bar

--- src/lib/components/SearchBar/SearchBarWithoutQueryAutosave.svelte
+++ src/lib/components/SearchBar/SearchBarWithoutQueryAutosave.svelte
@@ -2,6 +2,7 @@
 	import { generateQuery } from '$lib/components/Filters/utils/generateQuery';
 	import { queryStore, resetQuery } from '$lib/stores';
 	import { updateRecentQueries } from '$lib/utils/generateAndGo';
+	import { fade, scale } from 'svelte/transition';
 
 	let searchInput;
 	export let executeQuery: (query: string) => void;
@@ -30,18 +31,17 @@
 	const handleSearch = (e) => {
 		generateQueryAndGo();
 	};
-	
-	const handleSearchClear = (e) => {
-		// resetQuery()
-	};
 
+	const handleSearchClear = (e) => {
+		$queryStore.searchTerm = '';
+	};
 
 	function focusElement(element) {
 		element.focus();
 	}
 </script>
 
-<div class="relative w-full" >
+<div class="relative w-full">
 	<input
 		class="input input-bordered input-primary w-full"
 		placeholder="Search"
@@ -52,11 +52,16 @@
 		bind:this={searchInput}
 	/>
 	{#if $queryStore.searchTerm}
-	<button on:click={handleSearchClear} class="absolute top-0 right-0 rounded-l-none btn btn-error">X</button>
+		<button
+			on:click={handleSearchClear}
+			in:fade
+			out:scale
+			class="absolute top-0 right-0 rounded-l-none btn btn-error">X</button
+		>
 	{/if}
 </div>
 
 <button class="btn btn-primary" on:click={handleSearch}>Search</button>
 {#if $queryStore.searchTerm}
-	<button class="btn btn-outline" on:click={saveSearch}>Save</button>
+	<button class="btn btn-outline" in:fade on:click={saveSearch}>Save</button>
 {/if}
