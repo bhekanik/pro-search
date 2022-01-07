6ef403dc06be5f46f6cfc28b3f9a5d528711ed1f
Bhekani Khumalo <bhekanik@gmail.com>
2022-01-07 15:30:36 +0000

    Fix brocken reactivity in StoreMonitor component

--- src/lib/components/StoreMonitor/StoreMonitor.svelte
+++ src/lib/components/StoreMonitor/StoreMonitor.svelte
@@ -7,22 +7,35 @@
 		recentQueriesStore,
 		user
 	} from '$lib/stores';
+	import { onDestroy } from 'svelte';
 
 	let selected = 'featureFlags';
 
-	const flags = {
-		featureFlags: $featureFlagsStore,
-		auth: $user,
-		query: $queryStore,
-		recentQueries: $recentQueriesStore,
-		readiness: $readiness,
-		config: $configStore
+	const stores = {
+		featureFlags: { value: $featureFlagsStore, name: featureFlagsStore },
+		auth: { value: $user, name: user },
+		query: { value: $queryStore, name: queryStore },
+		recentQueries: { value: $recentQueriesStore, name: recentQueriesStore },
+		readiness: { value: $readiness, name: readiness },
+		config: { value: $configStore, name: configStore }
 	};
+
+	$: value = stores[selected].value;
+
+	let unsubscribe;
+
+	$: {
+		unsubscribe = stores[selected].name.subscribe((newValue) => {
+			value = newValue;
+		});
+	}
 
 	function handleClick(e) {
 		const { id } = e.target;
 		selected = id;
 	}
+
+	onDestroy(unsubscribe);
 </script>
 
 <div class="collapse z-50 my-2 border rounded-box border-base-300 collapse-arrow absolute top-0">
@@ -30,62 +43,19 @@
 	<div class="collapse-title text-xl font-medium">Store Monitor</div>
 	<div class="collapse-content pointer-events-none">
 		<div class="btn-group pointer-events-auto">
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="featureFlags"
-				data-title="featureFlags"
-				class="btn"
-				checked={selected === 'featureFlags'}
-			/>
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="auth"
-				data-title="auth"
-				class="btn"
-				checked={selected === 'auth'}
-			/>
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="query"
-				data-title="query"
-				class="btn"
-				checked={selected === 'query'}
-			/>
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="recentQueries"
-				data-title="recentQueries"
-				class="btn"
-				checked={selected === 'recentQueries'}
-			/>
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="config"
-				data-title="config"
-				class="btn"
-				checked={selected === 'config'}
-			/>
-			<input
-				on:click={handleClick}
-				type="radio"
-				name="options"
-				id="readiness"
-				data-title="readiness"
-				class="btn"
-				checked={selected === 'readiness'}
-			/>
+			{#each Object.keys(stores) as store}
+				<input
+					on:click={handleClick}
+					type="radio"
+					name="options"
+					id={store}
+					data-title={store}
+					class="btn"
+					checked={selected === store}
+				/>
+			{/each}
 		</div>
 
-		<pre class="bg-slate-800">{JSON.stringify(flags[selected], null, 2)}</pre>
+		<pre class="bg-slate-800">{JSON.stringify(value, null, 2)}</pre>
 	</div>
 </div>
