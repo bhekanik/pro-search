683338aa46d112a90037670dc0d30204817537ec
Guide Fari <guideg6@gmail.com>
2022-02-03 10:11:29 +0200

    Refactor: move content to SEO component

---
+++ src/lib/components/SEO/SEO.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Pro-Search</title>
+	<link rel="canonical" href="https://pro-search.io" />
+	<meta name="description" content="Advanced search query builder" />
+</svelte:head>
