c584216b2228ac25770e16a4f60d231c8a63f328
Bhekani Khumalo <bhekanik@gmail.com>
2022-02-04 03:04:15 +0000

    Add custom scrollbar for recipes grid

--- src/routes/recipes.svelte
+++ src/routes/recipes.svelte
@@ -21,7 +21,9 @@
 	class="p-8 pt-2 relative h-full overflow-y-auto grid grid-cols-[1fr] md:grid-cols-[minmax(600px,1fr)_minmax(290px,400px)] gap-8"
 >
 	{#if $readiness}
-		<div class="h-full grid gap-2 auto-rows-min grid-cols-[repeat(auto-fill,minmax(290px,1fr))]">
+		<div
+			class="h-full grid gap-2 auto-rows-min grid-cols-[repeat(auto-fill,minmax(290px,1fr))] overflow-auto recipes-list"
+		>
 			{#each recentQueries as query (query.id)}
 				<div
 					class="h-fit card glass hover:bg-blue-800 bg-blue-900 shadow-md py-2 px-4 relative"
@@ -45,3 +47,21 @@
 		</div>
 	{/if}
 </div>
+
+<style>
+	.recipes-list::-webkit-scrollbar {
+		width: 15px;
+	}
+
+	.recipes-list::-webkit-scrollbar-track {
+		background-color: #555555;
+		border-radius: 100px;
+	}
+
+	.recipes-list::-webkit-scrollbar-thumb {
+		border-radius: 80px;
+		border: 5px solid transparent;
+		background-clip: content-box;
+		background-color: #6174c3;
+	}
+</style>
