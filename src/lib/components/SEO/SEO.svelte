1e82363648b9e1fed2ef7cc6efcf8cfef90c5a1a
Bhekani Khumalo <bhekanik@gmail.com>
2022-02-09 23:00:39 +0000

    Add meta tags

--- src/lib/components/SEO/SEO.svelte
+++ src/lib/components/SEO/SEO.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { seo } from '$lib/stores';
+
 	let isProd = process.env.NODE_ENV === 'production';
+	const {
+		url: { pathname, host, protocol }
+	} = $page;
+	let url = `${protocol}//${host}${pathname}`;
 </script>
 
 <svelte:head>
 	<title>{$seo.title}</title>
 	<link rel="canonical" href="https://pro-search.io" />
 	<meta name="description" content={$seo.description} />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={$seo.title} />
+	<meta property="og:url" content={`${protocol}//${host}${pathname}`} />
+
+	<meta name="twitter:title" content={$seo.title} />
+	<meta name="twitter:description" content={$seo.description} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content={url} />
+	<meta name="twitter:site" content="@bhekanik" />
 
 	{#if isProd}
 		<script>
