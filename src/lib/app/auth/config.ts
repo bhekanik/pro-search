2cb9d5064570b74c8f0f34290626a0fd998496d1
Bhekani Khumalo <bhekanik@gmail.com>
2022-03-06 05:08:29 +0000

    Remove firebase and auth0

--- src/lib/app/auth/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-	domain: import.meta.env.VITE_AUTH0_DOMAIN as string,
-	clientId: import.meta.env.VITE_AUTH0_CLIENT_ID as string
-};
--- src/lib/components/AuthModal/AuthModal.svelte
+++ src/lib/components/AuthModal/AuthModal.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Auth from '$lib/components/SupabaseAuthUI/Auth.svelte';
-	import 'firebaseui/dist/firebaseui.css';
 
 	export let closeModalButton = null;
 	export let redirectTo = '';
@@ -10,7 +9,6 @@
 <div class="modal">
 	<div class="modal-box">
 		<h3 class="font-bold text-lg p-8">Login/Register</h3>
-		<!-- <div id="firebaseui-auth-container" class="w-full" /> -->
 		<Auth providers={['google', 'github']} {redirectTo} />
 		<div class="modal-action px-8 pb-8">
 			<label for="auth-modal" bind:this={closeModalButton} class="btn w-full">Close</label>
